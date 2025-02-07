4dfb101c366b41a4ab7ab0b45717909a1d472552
Filip Biernat <fbiernat@gmail.com>
2025-02-07 20:08:50 +0100

    Move script 1 level down

--- RScraper/RScraper.py
+++ RScraper/RScraper.py
@@ -3,14 +3,15 @@ import json
 from scraper import get_dates_and_prices
 from processor import process_data
 
-def read_urls_from_json(json_file):
-    print(f"Reading URLs from JSON file: {json_file}")
-    with open(json_file, 'r', encoding='utf-8') as f:
+def read_urls_from_json(json_file_path):
+    print(f"Reading URLs from JSON file: {json_file_path}")
+    with open(json_file_path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 if __name__ == "__main__":
-    json_file = "sources.json"
-    url_data = read_urls_from_json(json_file)
+    json_file_name = "sources.json"
+    json_file_path = os.path.join(os.pardir, json_file_name)
+    url_data = read_urls_from_json(json_file_path)
 
     for name, details in url_data.items():
         link = details["link"]
@@ -27,5 +28,5 @@ if __name__ == "__main__":
         if not os.path.exists("data"):
             os.makedirs("data")
 
-        file_path = os.path.join("data", f"{name}.csv")
+        file_path = os.path.join(os.pardir, "data", f"{name}.csv")
         process_data(results, file_path)
